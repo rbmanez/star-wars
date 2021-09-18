2693eaa201f7dfc8f6daef256ce1466103acd808
Robert <rbmanez@gmail.com>
2021-09-18 11:05:01 -0600

    update with hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Characters from './components/Characters'
 import Films from './components/Films'
